02bad6c52447d4adb5cf1a10343ad21afa2b8349
rochdev <roch.devost@gmail.com>
2024-08-06 14:42:04 -0400

    code cleanup

--- src/crashtracker.rs
+++ src/crashtracker.rs
@@ -13,20 +13,40 @@ pub fn register (cx: &mut ModuleContext) -> NeonResult<()> {
 pub fn start (mut cx: FunctionContext) -> JsResult<JsUndefined> {
     // TODO: get actual config from cx
 
+    let additional_files = vec![];
+    let create_alt_stack = false;
     let uri = Uri::from_static("http://localhost:8126");
-    let endpoint = Endpoint::from_url(uri);
-    let resolve_frames = crashtracker::StacktraceCollection::Disabled;
+    let endpoint = Some(Endpoint::from_url(uri));
+    let resolve_frames = StacktraceCollection::Disabled;
     let wait_for_receiver = false;
-    let config = CrashtrackerConfiguration::new(vec![], false, Some(endpoint), resolve_frames, wait_for_receiver).unwrap();
+    let config = CrashtrackerConfiguration::new(
+        additional_files,
+        create_alt_stack,
+        endpoint,
+        resolve_frames,
+        wait_for_receiver,
+    ).unwrap();
 
+    let args = vec![];
+    let env = vec![];
     let path_to_receiver_library = "/tmp/foo".to_string();
-    let receiver_config = crashtracker::CrashtrackerReceiverConfig::new(vec![], vec![], path_to_receiver_library, None, None).unwrap();
+    let stderr_filename = None;
+    let stdout_filename = None;
+    let receiver_config = CrashtrackerReceiverConfig::new(
+        args,
+        env,
+        path_to_receiver_library,
+        stderr_filename,
+        stdout_filename,
+    ).unwrap();
 
     let library_name = "dd-trace-js".to_string();
     let library_version = "0.0.0".to_string();
-    let metadata = crashtracker::CrashtrackerMetadata::new(library_name, library_version, "nodejs".to_string(), vec![]);
+    let family = "nodejs".to_string();
+    let tags = vec![];
+    let metadata = CrashtrackerMetadata::new(library_name, library_version, family, tags);
 
-    crashtracker::init_with_receiver(config, receiver_config, metadata).unwrap();
+    init_with_receiver(config, receiver_config, metadata).unwrap();
 
     Ok(cx.undefined())
 }
