7d1f3f98775b6bb94430f71de17583199a6f97ba
rochdev <roch.devost@gmail.com>
2024-08-12 19:09:40 -0400

    code cleanup

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const load = require('./load')
 
 module.exports = {
-  get collector () { return load ('collector') },
-  get pipeline () { return load ('pipeline')}
+  get collector () { return load('collector') },
+  get pipeline () { return load('pipeline')}
 }
