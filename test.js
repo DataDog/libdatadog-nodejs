d6a9127877d305499b9fb0ef0aec525ee0dddb64
rochdev <roch.devost@gmail.com>
2024-11-07 19:07:22 -0500

    debug

--- test.js
+++ test.js
@@ -3,6 +3,9 @@
 const fs = require('fs')
 const { execSync } = require('child_process')
 
+execSync('touch baz.txt')
+execSync('touch test/baz.txt')
+execSync('touch test/empty/baz.txt')
 execSync('touch test/crashtracker/baz.txt')
 
 fs.readdirSync('test')
