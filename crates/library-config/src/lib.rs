0e14199e64ee93cd8944ce24f558a549c4543587
Baptiste Foy <baptiste.foy@datadoghq.com>
2025-01-08 16:40:21 +0100

    switch to wasm

--- crates/library-config/src/lib.rs
+++ crates/library-config/src/lib.rs
@@ -1,9 +1,10 @@
+use std::collections::HashMap;
+
 use wasm_bindgen::prelude::*;
-use datadog_library_config_ffi::slice;
 
 #[wasm_bindgen]
 pub struct JsConfigurator {
-    configurator: Box<datadog_library_config_ffi::static_config::Configurator>,
+    configurator: Box<datadog_library_config::Configurator>,
     envp: Vec<String>,
     args: Vec<String>,
 }
@@ -13,7 +14,7 @@ impl JsConfigurator {
     #[wasm_bindgen(constructor)]
     pub fn new(debug_logs: bool) -> Self {
         JsConfigurator {
-            configurator: datadog_library_config_ffi::ddog_library_configurator_new(debug_logs),
+            configurator: Box::new(datadog_library_config::Configurator::new(debug_logs)),
             envp: Vec::new(),
             args: Vec::new(),
         }
@@ -40,57 +41,35 @@ impl JsConfigurator {
         &self,
         config_string: String,
     ) -> Result<JsValue, JsValue> {
-        let envp_slices: Vec<slice::CharSlice> = self
+        let envp: Vec<&[u8]> = self
             .envp
             .iter()
-            .map(|s| slice::CharSlice::from(s.as_str()))
+            .map(|s| s.as_bytes())
             .collect();
-        let envp_slice = slice::Slice::from(&envp_slices);
 
-        let args_slices: Vec<slice::CharSlice> = self
+        let args: Vec<_> = self
             .args
             .iter()
-            .map(|s| slice::CharSlice::from(s.as_str()))
+            .map(|s| s.as_bytes())
             .collect();
-        let args_slice = slice::Slice::from(&args_slices);
 
-        let res_config = self.configurator.get_configuration_from_bytes(
-            datadog_library_config_ffi::static_config::ProcessInfo {
-                envp: envp_slice,
-                args: args_slice,
-                language: slice::CharSlice::from("nodejs"),
+        let res_config = self.configurator.get_config_from_bytes(
+            config_string.as_bytes(),
+            datadog_library_config::ProcessInfo {
+                envp: &envp,
+                args: &args,
+                language: b"nodejs",
             },
-            slice::CharSlice::from(config_string.as_str()),
         );
 
         match res_config {
             Ok(config) => {
-                let mut hashmap = std::collections::HashMap::new();
-                config.iter().for_each(|c| {
-                    let key = String::from_utf8_lossy(c.name.to_env_name().to_bytes()).to_string();
-                    let value: String;
-                    match &c.value {
-                        datadog_library_config_ffi::static_config::LibraryConfigValue::StrVal(
-                            v,
-                        ) => {
-                            value = String::from_utf8_lossy(v.as_cstr().into_std().to_bytes())
-                                .to_string();
-                        }
-                        datadog_library_config_ffi::static_config::LibraryConfigValue::NumVal(
-                            v,
-                        ) => {
-                            value = v.to_string();
-                        }
-                        datadog_library_config_ffi::static_config::LibraryConfigValue::BoolVal(
-                            v,
-                        ) => {
-                            value = v.to_string();
-                        }
-                    }
-                    hashmap.insert(key, value);
-                });
+                let hashmap: HashMap<String, String> = config.into_iter().map(|c| {
+                    let key = c.name.to_str().to_owned();
+                    (key, c.value)
+                }).collect();
                 Ok(serde_wasm_bindgen::to_value(&hashmap)?)
-            }
+            },
             Err(e) => Err(JsValue::from_str(&format!(
                 "Failed to get configuration: {:?}",
                 e
--- test/library-config/README.md
+++ test/library-config/README.md
@@ -1,13 +1,6 @@
 # Libconfig example
 
 ## How to run
-Set the following env vars:
-```bash
-export OS=$(uname | tr '[:upper:]' '[:lower:]') # darwin / linux / windows
-export ARCH=$(arch) # arm64 / amd64
-export LIBC='' # glibc / musl if on linux, else empty
-```
-
 From repository root 
 ```bash
 wasm-pack build ./crates/library-config
