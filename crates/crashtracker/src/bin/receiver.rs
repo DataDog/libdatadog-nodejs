23246169f024bd679e28850611a92c10a0e5788b
Gyuheon Oh <gyuheon.oh@datadoghq.com>
2026-02-05 10:40:50 -0500

    Remove crypto init

--- crates/crashtracker/src/bin/receiver.rs
+++ crates/crashtracker/src/bin/receiver.rs
@@ -3,7 +3,5 @@ fn main() {}
 
 #[cfg(unix)]
 fn main() -> anyhow::Result<()> {
-    // TODO: remove this line when the receiver default provider is fixed
-    rustls::crypto::aws_lc_rs::default_provider().install_default().unwrap();
     libdd_crashtracker::receiver_entry_point_stdin()
 }
