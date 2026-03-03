4c29705223c41fc7d4270f78a445761b1a0055d8
Gyuheon Oh <gyuheon.oh@datadoghq.com>
2026-03-03 09:36:33 -0500

    Revert "V0.8.2 proposal (#76)"
    This reverts commit f3e9b05b1dad4c06889075d267d8b34d0dec76f2.

--- test/crashtracker/app-seg-fault.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const libdatadog = require('../..')
-const crashtracker = libdatadog.load('crashtracker')
-const { initTestCrashtracker } = require('./test_utils')
-
-initTestCrashtracker()
-crashtracker.beginProfilerSerializing()
-require('@datadog/segfaultify').segfaultify()
