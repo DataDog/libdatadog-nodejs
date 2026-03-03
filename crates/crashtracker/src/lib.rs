7a19a220d1e29c3a1a79f0c86d97e5b718ff7eca
Gyuheon Oh <gyuheon.oh@datadoghq.com>
2026-03-03 09:36:33 -0500

    Revert "V0.8.2 proposal (#76)"
    This reverts commit f3e9b05b1dad4c06889075d267d8b34d0dec76f2.

--- crates/crashtracker/src/lib.rs
+++ crates/crashtracker/src/lib.rs
@@ -1,11 +1,9 @@
 use napi::{Env, JsUnknown};
 use napi_derive::napi;
 
-mod unhandled_exception;
-
 /// Ensures that if signals is empty, default signals are applied.
 /// This is necessary because NAPI deserialization bypasses the
-/// CrashtrackerConfiguration::new() constructor where the default
+/// CrashtrackerConfiguration::new() constructor where the default 
 /// signals logic exists.
 fn apply_default_signals(
     config: libdd_crashtracker::CrashtrackerConfiguration,
@@ -17,7 +15,7 @@ fn apply_default_signals(
             config.use_alt_stack(),
             config.endpoint().clone(),
             config.resolve_frames(),
-            vec![], // Empty vec will be replaced with default_signals() in new() in libdatadog
+            vec![],  // Empty vec will be replaced with default_signals() in new() in libdatadog
             Some(config.timeout()),
             config.unix_socket_path().clone(),
             config.demangle_names(),
@@ -29,12 +27,7 @@ fn apply_default_signals(
 }
 
 #[napi]
-pub fn init(
-    env: Env,
-    config: JsUnknown,
-    receiver_config: JsUnknown,
-    metadata: JsUnknown,
-) -> napi::Result<()> {
+pub fn init(env: Env, config: JsUnknown, receiver_config: JsUnknown, metadata: JsUnknown) -> napi::Result<()> {
     let config: libdd_crashtracker::CrashtrackerConfiguration = env.from_js_value(config)?;
     let receiver_config = env.from_js_value(receiver_config)?;
     let metadata = env.from_js_value(metadata)?;
@@ -47,7 +40,7 @@ pub fn init(
 }
 
 #[napi]
-pub fn update_config(env: Env, config: JsUnknown) -> napi::Result<()> {
+pub fn update_config (env: Env, config: JsUnknown) -> napi::Result<()> {
     let config: libdd_crashtracker::CrashtrackerConfiguration = env.from_js_value(config)?;
 
     let config = apply_default_signals(config);
@@ -58,7 +51,7 @@ pub fn update_config(env: Env, config: JsUnknown) -> napi::Result<()> {
 }
 
 #[napi]
-pub fn update_metadata(env: Env, metadata: JsUnknown) -> napi::Result<()> {
+pub fn update_metadata (env: Env, metadata: JsUnknown) -> napi::Result<()> {
     let metadata = env.from_js_value(metadata)?;
 
     libdd_crashtracker::update_metadata(metadata).unwrap();
@@ -67,14 +60,14 @@ pub fn update_metadata(env: Env, metadata: JsUnknown) -> napi::Result<()> {
 }
 
 #[napi]
-pub fn begin_profiler_serializing(_env: Env) -> napi::Result<()> {
+pub fn begin_profiler_serializing (_env: Env) -> napi::Result<()> {
     let _ = libdd_crashtracker::begin_op(libdd_crashtracker::OpTypes::ProfilerSerializing);
 
     Ok(())
 }
 
 #[napi]
-pub fn end_profiler_serializing(_env: Env) -> napi::Result<()> {
+pub fn end_profiler_serializing (_env: Env) -> napi::Result<()> {
     let _ = libdd_crashtracker::end_op(libdd_crashtracker::OpTypes::ProfilerSerializing);
 
     Ok(())
