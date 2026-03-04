3900d3f1c4dc036b22ad780380b5432d54def0f8
Gyuheon Oh <gyuheon.oh@datadoghq.com>
2026-03-04 23:30:45 +0000

    Out of process symbolization

--- test/crashtracker/test-utils.js
+++ test/crashtracker/test-utils.js
@@ -17,7 +17,7 @@ function initTestCrashtracker () {
       timeout_ms: 3000,
     },
     timeout: { secs: 15, nanos: 0 },
-    resolve_frames: 'EnabledWithInprocessSymbols',
+    resolve_frames: 'EnabledWithSymbolsInReceiver',
     wait_for_receiver: true,
     demangle_names: false,
     signals: [],
