7d4a86aef1431178f1b6cd59458b7d1462f707f5
Ilyas Shabi <ilyasshabi94@gmail.com>
2026-08-10 17:25:55 +0200

    feat: add DDSketch WASM bindings (#198)
    * feat: add DDSketch WASM bindings

---
+++ test/wasm/sketches/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const assert = require('node:assert')
+
+const loader = require('../../../load')
+const { DDSketch } = loader.load('sketches')
+
+const sketch = new DDSketch()
+assert.strictEqual(sketch.count(), 0)
+
+sketch.add(1)
+sketch.addWithCount(2, 3)
+assert.strictEqual(sketch.count(), 4)
+
+assert.throws(() => sketch.add(-1), /point is invalid/)
+assert.throws(() => sketch.addWithCount(1, Number.NaN), /count is invalid/)
+
+const encoded = sketch.encode()
+assert(encoded instanceof Uint8Array)
+assert(encoded.length > 0)
+
+assert.strictEqual(sketch.count(), 4)
+sketch.add(3)
+assert.strictEqual(sketch.count(), 5)
+
+const reencoded = sketch.encode()
+assert(reencoded instanceof Uint8Array)
+assert(reencoded.length > 0)
+assert.strictEqual(sketch.count(), 5)
